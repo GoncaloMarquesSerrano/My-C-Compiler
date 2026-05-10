0f18bc8f2e77c3b603587c37b40fb9dda9b2a999
gs <goncaloserrano2005@gmail.com>
2026-05-10 22:31:43 +0100

    feat: stub run_compiler function in compiler driver

--- src/main.c
+++ src/main.c
@@ -25,13 +25,47 @@ void build_filenames(const char *input, char *preprocessed, char *assembly,
   snprintf(executable, buf_size, "%s", filename);
 }
 
-int run_compiler(const char *input_file, const char *flag) { return 0; }
+int run_compiler(const char *input_file, const char *flag) {
+  char preprocessed[512], assembly[512], executable[512];
+  build_filenames(input_file, preprocessed, assembly, executable, 512);
+
+  // step 1: preprocess
+  char cmd[1024];
+  snprintf(cmd, sizeof(cmd), "gcc -E -P %s -o %s", input_file, preprocessed);
+  if (system(cmd) != 0) {
+    fprintf(stderr, "Error preprocessing file");
+    return 1;
+  }
+
+  // step 2: compile
+  // stubbing out for now
+
+  printf("Compiling...");
+
+  // if any flag is given doesnt do anything
+  if (strlen(flag) > 0) {
+    if (remove(preprocessed) != 0)
+      fprintf(stderr, "Error deleting preprocessed file");
+    return 0;
+  }
+
+  if (remove(preprocessed) != 0) {
+    fprintf(stderr, "Error deleting preprocessed file");
+  }
+
+  // step 3: assemble and link to produce executable
+  /*
+  snprintf(cmd, sizeof(cmd), "gcc %s -o %s", assembly, executable);
+  if (system(cmd) != 0) {
+    fprintf(stderr, "Error linking assembly file");
+    return 1;
+  }
+  */
+  remove(assembly);
+  return 0;
+}
 
 int main(int argc, char *argv[]) {
-
-  char preprocessed[512];
-  char assembly[512];
-  char executable[512];
 
   if (argc == 1) {
     fprintf(stderr, "use: ./compiler [--lex|--parse|--codegen]");
@@ -40,30 +74,17 @@ int main(int argc, char *argv[]) {
   if (argc == 2) {
     // file is argv[1]
     // call compiler
-    build_filenames(argv[1], preprocessed, assembly, executable,
-                    sizeof(preprocessed));
-    run_compiler(argv[1], "");
+    return run_compiler(argv[1], "");
   }
   if (argc == 3) {
     // file is argv[2]
-    build_filenames(argv[2], preprocessed, assembly, executable,
-                    sizeof(preprocessed));
-
     // find out which flag is being used
-    if (strcmp(argv[1], "--lex") == 0) {
-      run_compiler(argv[2], "--lex");
-      return 0;
-    } else if (strcmp(argv[1], "--parse") == 0) {
-      run_compiler(argv[2], "--parse");
-      return 0;
-    } else if (strcmp(argv[1], "--codegen") == 0) {
-      run_compiler(argv[2], "--codegen");
-      return 0;
-    } else {
-      fprintf(stderr, "use: ./compiler [--lex|--parse|--codegen]");
-      exit(1);
-    };
+    if (strcmp(argv[1], "--lex") == 0 || strcmp(argv[1], "--parse") == 0 ||
+        strcmp(argv[1], "--codegen") == 0) {
+      return run_compiler(argv[2], argv[1]);
+    }
+    fprintf(stderr, "use: ./compiler [--lex|--parse|--codegen]");
+    exit(1);
   }
-
   return 0;
 }
