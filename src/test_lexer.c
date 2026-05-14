19c8a61f5fc8b394adf80e2ef05f015b7a1de9b6
gs <goncaloserrano2005@gmail.com>
2026-05-14 17:58:17 +0100

    test: add test_lexer for TokenList and lexer_tokenize validation

---
+++ src/test_lexer.c
@@ -0,0 +1,15 @@
+#include "lexer.h"
+#include "token.h"
+#include <stdio.h>
+
+int main(void) {
+  TokenList list = lexer_tokenize("int main(void) { return 2; }");
+
+  for (size_t i = 0; i < list.size; i++) {
+    Token t = list.tokens[i];
+    printf("type=%d value=%s\n", t.type, t.value ? t.value : "(null)");
+  }
+
+  token_list_free(&list);
+  return 0;
+}
